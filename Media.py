fba1dda37d904bac988b8b6220f199bfb1247fc2
Ibaisolves <solvescuencaibai@gmail.com>
2025-09-19 08:58:07 +0200

    Add files via upload

---
+++ Media.py
@@ -0,0 +1,6 @@
+n1 = int(input('Dame el primer numero: '))
+n2 = int(input('Dame el segundo numero: '))
+n3 = int(input('Dame el tercer numero: '))
+n4 = int(input('Dame el cuarto numero: '))      
+media = (n1 + n2 + n3 + n4) / 4
+print(f'La media de los numeros es: {media}')
