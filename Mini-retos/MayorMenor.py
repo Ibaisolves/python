4d1c53ace3b3cdb7039ce1a82f14b079e766f900
Ibaisolves <solvescuencaibai@gmail.com>
2025-10-20 11:55:05 +0200

    Add files via upload

---
+++ Mini-retos/MayorMenor.py
@@ -0,0 +1,26 @@
+nums= [6, 3, 7, 2, 9, 4, 6, 6, 1, 3, 2, 8, 5]
+
+# Suma de los numeros
+print("Suma de los numeros:", sum(nums))
+
+# Maximo de los numeros
+print("Máximo de los numeros:", max(nums))
+
+# Ocurriencia
+repite=6
+for x in nums:
+    if x == nums:
+    i +=1
+print("El numero %d se repite %d", repite, i)  
+
+#Lista de pares
+pares = 0
+for x in nums:
+    if x % 2 == 0:
+        pares += 1
+
+# Hallar minimos y maximos 
+for
+    
+  
+
